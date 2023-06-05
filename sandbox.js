fd293a4c545e8947c2e846c901cdb3a67ae3b419
jojo sexton <jojolsexton@gmail.com>
2023-06-06 00:04:15 +1200

    fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,43 @@
-//a promise is a function go fetch something and returns 2 outcomes resolve or reject.
+fetch('todos/poppy.json').then(response =>{
+  console.log('resolved',response)
+  return response.json().then((data) => {
+    console.log(data)
+  }
+  )
+}).catch (err => {
+  console.log('could not get data', err)
+})
 
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
 
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-        resolve(data)
-      } else if (request.readyState === 4) {
-        reject('error getting resource')
-      }
-    })
-    request.open('GET', resource)
-    request.send()
-  })
-}
 
-getTodos('todos/poppy.json')
-  .then((data) => {
-    console.log('promise 1 resoved', data)
-    //second task
-    return getTodos('todos/mum.json').then((data) => {
-      console.log('promise 2 resolved', data)
-      return getTodos('todos/dad.json').then((data) => {
-        console.log('promise 3 resolved', data)
-      })
-    })
-  })
-  .catch((err) => console.log('promise rejected', err))
+// //a promise is a function go fetch something and returns 2 outcomes resolve or reject.
+
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest()
+
+//     request.addEventListener('readystatechange', () => {
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText)
+//         resolve(data)
+//       } else if (request.readyState === 4) {
+//         reject('error getting resource')
+//       }
+//     })
+//     request.open('GET', resource)
+//     request.send()
+//   })
+// }
+
+// getTodos('todos/poppy.json')
+//   .then((data) => {
+//     console.log('promise 1 resoved', data)
+//     //second task
+//     return getTodos('todos/mum.json').then((data) => {
+//       console.log('promise 2 resolved', data)
+//       return getTodos('todos/dad.json').then((data) => {
+//         console.log('promise 3 resolved', data)
+//       })
+//     })
+//   })
+//   .catch((err) => console.log('promise rejected', err))
